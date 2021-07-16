e1effd0c31398b910496697631e6635c3ae8f918
Aglowkeys <emilianoalfonso@outlook.com>
2021-07-15 23:51:18 -0300

    /items/:id devuelve también categorías

--- api/src/routes/items.js
+++ api/src/routes/items.js
@@ -23,12 +23,19 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
 	const { id } = req.params;
 	const requests = [axios.get(`/items/${id}`), axios.get(`/items/${id}/description`)];
+	let formattedProduct = {};
 
 	Promise.all(requests)
 		.then((dataArray) => {
 			const product = dataArray[0].data;
 			const productDescription = dataArray[1].data.plain_text;
-			const formattedProduct = getObjectWithDescription(product, productDescription);
+			formattedProduct = getObjectWithDescription(product, productDescription);
+			return axios.get(`/categories/${product.category_id}`);
+		})
+		.then((categoryData) => {
+			const { path_from_root } = categoryData.data;
+			const categories = path_from_root.map((cat) => cat.name);
+			formattedProduct['categories'] = categories;
 			return res.json(formattedProduct);
 		})
 		.catch((error) => res.status(500).json({ error }));
