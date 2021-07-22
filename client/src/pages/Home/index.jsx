6a5670fee28cac133c9daac0967cb52e44bdfe4d
Aglowkeys <emilianoalfonso@outlook.com>
2021-07-21 21:51:26 -0300

    Agregado svg al componente Home

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import SinglePageLayout from '../../components/SinglePageLayout';
 import Head from '../../components/Head';
+import ShoppingImage from './ShoppingImage';
 
 const Home = () => (
     <>
         <Head title='Inicio | Mercado Libre' />
         <SinglePageLayout>
+            <ShoppingImage />
             <h2>¡Bienvenido a la página de Mercado Libre!</h2>
             <p>Encuentra productos utilizando el buscador de arriba.</p>
         </SinglePageLayout>
